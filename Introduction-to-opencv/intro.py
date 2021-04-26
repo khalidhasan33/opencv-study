56c70a78fb92643289072b56769d8682d993ced2
Khalid <sawmaster22@gmail.com>
2021-04-26 23:34:57 +0700

    get image size

---
+++ Introduction-to-opencv/intro.py
@@ -0,0 +1,9 @@
+# Importing the OpenCV library
+import cv2
+# Reading the image using imread() function
+image = cv2.imread('image.jpg')
+  
+# Extracting the height and width of an image
+h, w = image.shape[:2]
+# Displaying the height and width
+print("Height = {},  Width = {}".format(h, w))
